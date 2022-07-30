552e9275801596e2b86b1af07434236aad7e00c5
robsonshockwave <robsonshockwavegithub@gmail.com>
2022-07-30 15:02:26 -0300

    comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from './Comment.module.css';
+import { Trash, ThumbsUp } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/robsonshockwave.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Robson Arruda</strong>
+              <time title="30 de Julho às 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca há 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
